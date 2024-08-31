40b792e478ac5ea7541d3715b9f9019fa04049d4
muhammadilham730 <mi8190044@gmail.com>
2024-08-31 11:57:43 +0700

    05 perulangan bersarang

---
+++ 05 perulangan bersarang.php
@@ -0,0 +1,12 @@
+
+<?php
+$i = 0;
+while($i < 10){
+    for($j = 0; $j < 12; $j++){
+        echo "Ini perulangan ke ($i, $j)<br>";
+    }
+
+    $i++;
+}
+
+print_r("<p><strong><i>By Muhammad ilham!</strong>");
