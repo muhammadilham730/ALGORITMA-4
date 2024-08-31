a3963b5ef53c4c1df94fd90d0db168118bf2c5d7
muhammadilham730 <mi8190044@gmail.com>
2024-08-31 11:50:41 +0700

    03while

--- 03 while.php
+++ 03 while.php
@@ -6,5 +6,5 @@ do {
     echo "<p>ini adalah perulangan ke-$ulangi</p>";
     $ulangi--;
 } while ($ulangi > 0);
-print_r("<p><strong><i> by muhammad ilham</strong>");
+print_r("<p><strong><i> by muhammad ilhammm</strong>");
 ?>
