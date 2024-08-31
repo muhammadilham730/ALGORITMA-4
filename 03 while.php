6a6bbc7bf8fcf90cefc8305ee2a176c815825fb4
muhammadilham730 <mi8190044@gmail.com>
2024-08-31 11:47:13 +0700

    03 while

---
+++ 03 while.php
@@ -0,0 +1,10 @@
+<?php 
+
+$ulangi = 10;
+
+do {
+    echo "<p>ini adalah perulangan ke-$ulangi</p>";
+    $ulangi--;
+} while ($ulangi > 0);
+print_r("<p><strong><i> by muhammad ilham</strong>");
+?>
