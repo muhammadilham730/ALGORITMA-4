fddc385319ffdc1304c60ba500c3b318524644bc
muhammadilham730 <mi8190044@gmail.com>
2024-08-31 12:19:03 +0700

    09 pernyataan foreach

---
+++ 09 pernyataan foreach.php
@@ -0,0 +1,9 @@
+
+<?php
+
+// Kode yang akan dijalankan
+$warna = array('merah', 'hijau', 'biru');
+foreach ($warna as $w) {
+  echo "Warna: $w";
+}
+print_r("<p><strong><i>By Muhammad ilham</strong>");
