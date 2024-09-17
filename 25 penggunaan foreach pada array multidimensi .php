7ebac6e42bd53b49cc228b22bb900aa8adad763a
muhammadilham730 <mi8190044@gmail.com>
2024-09-17 11:11:45 +0700

    25 penggunaan foreach pada array multidimensi

---
+++ 25 penggunaan foreach pada array multidimensi .php
@@ -0,0 +1,15 @@
+<?php
+$matriks = array(
+   array(1, 2, 3),
+   array(4, 5, 6),
+   array(7, 8, 9)
+);
+
+foreach ($matriks as $baris) {
+    foreach ($baris as $nilai) {
+        echo $nilai . " ";
+    }
+    echo "<br>";
+}
+
+echo "<p><strong><i>By Muhammad ilham</strong>";
