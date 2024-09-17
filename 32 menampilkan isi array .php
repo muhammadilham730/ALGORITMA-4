83eae1d2c1047b5a5859dfb1cafd1ddd964a55d0
muhammadilham730 <mi8190044@gmail.com>
2024-09-17 13:27:11 +0700

    32 menampilkan isi array

---
+++ 32 menampilkan isi array .php
@@ -0,0 +1,10 @@
+<?php
+// membuat array
+$barang = ["Buku Tulis", "Penghapus", "Spidol"];
+
+// menampilkan isi array
+echo $barang[0]."<br>";
+echo $barang[1]."<br>";
+echo $barang[2]."<br>";
+
+echo "<p><strong><i>By Muhammad ilham</strong>";
