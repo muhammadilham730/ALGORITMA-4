0153a28ec8b6d838005b2d9b553c583c0aa421c0
muhammadilham730 <mi8190044@gmail.com>
2024-09-17 11:05:59 +0700

    24 foreach dengan mendapatkan key dari array

---
+++ 24 foreach dengan mendapatkan key dari array .php
@@ -0,0 +1,8 @@
+<?php
+$harga = array("apel" => 10000, "pisang" => 5000, "ceri" => 15000);
+
+foreach ($harga as $kunci => $nilai) {
+    echo "Harga dari " . $kunci . " adalah " . $nilai . "<br>";
+}
+
+echo "<p><strong><i>By Muhammad ilham</strong>";
