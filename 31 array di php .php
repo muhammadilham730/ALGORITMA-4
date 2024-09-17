8bd1311e59b00dec8904f526f8d98a8f717abf75
muhammadilham730 <mi8190044@gmail.com>
2024-09-17 12:26:37 +0700

    31 array di php

---
+++ 31 array di php .php
@@ -0,0 +1,13 @@
+<?php
+//1)	Membuat array kosong
+$buah = array();
+$hobi = [];
+//2)	Membuat array sekaligus mengisinya
+$minuman = array("Kopi", "Teh", "Jus Jeruk");
+$makanan = ["Nasi Goreng", "Soto", "Bubur"];
+//3)	Membuat array dengan mengisi indeks tertentu
+$anggota[1] = "Dian";
+$anggota[2] = "Muhar";
+$anggota[0] = "Meylisa";
+
+echo "<p><strong><i>By Muhammad ilham </strong>";
