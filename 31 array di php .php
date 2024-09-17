d6886bf22ac6406e9b10718927cf4d18295d68c2
muhammadilham730 <mi8190044@gmail.com>
2024-09-17 12:30:02 +0700

    31 array di php

--- 31 array di php .php
+++ 31 array di php .php
@@ -8,6 +8,6 @@ $makanan = ["Nasi Goreng", "Soto", "Bubur"];
 //3)	Membuat array dengan mengisi indeks tertentu
 $anggota[1] = "Dian";
 $anggota[2] = "Muhar";
-$anggota[0] = "Meylisa";
+$anggota[0] = "Muhammad ilham";
 
 echo "<p><strong><i>By Muhammad ilham </strong>";
