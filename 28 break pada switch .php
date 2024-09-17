c92b588e9821cc3144bd38f7fe194d450b59d087
muhammadilham730 <mi8190044@gmail.com>
2024-09-17 11:20:36 +0700

    28 break pada switch

---
+++ 28 break pada switch .php
@@ -0,0 +1,19 @@
+<?php
+$buah = "apel";
+
+switch ($buah) {
+    case "mangga":
+        echo "Ini mangga";
+        break;
+    case "apel":
+        echo "Ini apel";
+        // Break disini menghentikan switch
+        break;
+    case "pisang":
+        echo "Ini pisang";
+        break;
+    default:
+        echo "Buah tidak dikenal";
+}
+
+echo "<p><strong><i>By Muhammad ilham</strong>";
