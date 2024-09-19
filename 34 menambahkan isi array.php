674d4db5bd6d41ddad5ce9007b5a178f65527980
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 08:13:33 +0700

    34 menambah kan isi array

---
+++ 34 menambahkan isi array.php
@@ -0,0 +1,21 @@
+
+<?php
+// membuat array
+$hobi = [
+    "Membaca",
+    "Menulis",
+    "Ngeblog"
+];
+
+// menambahkan isi pada idenks ke-3
+$hobi[3] = "Coding";
+
+// menambahkan isi pada indeks terakhir
+$hobi[] = "Olahraga";
+
+// cetak array dengan perulangan
+foreach($hobi as $hobiku){
+    echo $hobiku."<br>";
+}
+
+echo "<p><i><strong> By muhammad ilham</strong>";
