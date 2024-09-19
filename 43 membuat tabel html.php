f1239a27cb3186e1946749b6a6fa547811099081
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 08:46:57 +0700

    43 membuat tabel html

---
+++ 43 membuat tabel html.php
@@ -0,0 +1,50 @@
+<?php
+$no = 1;
+$tabel = '
+<table>
+	<tr>
+		<th>No</th>
+		<th>Bulan</th>
+		<th>Penjualan</th>
+	</tr>';
+{
+	$tabel .= '
+	<tr>
+	</tr>';
+	$no++;
+}
+$tabel .= '</table>';
+echo $tabel;
+
+?>
+
+<table>
+	
+	<tr>
+		<td>1</td>
+		<td>Januari</td>
+		<td>5.500</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>Februari</td>
+		<td>7.500</td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td>Maret</td>
+		<td>11.500</td>
+	</tr>
+	<tr>
+		<td>4</td>
+		<td>April</td>
+		<td>8.800</td>
+	</tr>
+	<tr>
+		<td>5</td>
+		<td>Mei</td>
+		<td>7.500</td>
+	</tr>
+</table>
+
+<p><strong>By muhammad ilham</strong>
