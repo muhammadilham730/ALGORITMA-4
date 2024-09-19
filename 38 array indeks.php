f92bb1886f89bfa093034ecfb9a6545ffaf8cf04
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 08:30:42 +0700

    38 array indeks

---
+++ 38 array indeks.php
@@ -0,0 +1,8 @@
+
+<?php
+  $nama_variabel=array("nama1"=>"70","nama2"=>"67","nama3"=>"89");
+  echo "hasil dari program " . $nama_variabel['nama1'] . " nilai.";
+
+
+echo "<p><strong>By muhammad ilham</strong>";
+  ?>
