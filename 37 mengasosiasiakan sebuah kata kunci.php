116c573bd0354c0bc5ccd355bc78499864203460
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 08:28:13 +0700

    37 mengasosiasikan sebuah kata kunci

---
+++ 37 mengasosiasiakan sebuah kata kunci.php
@@ -0,0 +1,11 @@
+
+<?php
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "ilham smk1karangbaru@gmail.com";
+$email["isi"] = "Apa kabar ilham? sudah lama kita tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
+
+echo "<p><strong>By muhammad ilham</strong>";
