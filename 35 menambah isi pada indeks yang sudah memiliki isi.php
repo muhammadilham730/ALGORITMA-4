cf6cb92e8809af00fc1d89e820711374e396da66
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 08:18:50 +0700

    35 menambah isi pada indeks yang sudah memiliki isi

---
+++ 35 menambah isi pada indeks yang sudah memiliki isi.php
@@ -0,0 +1,20 @@
+
+<?php
+// membuat array
+$user = [
+    "Lisa",
+    "muhar",
+    "amellya"
+];
+
+// mengisi array pada indek ke-1 ("muhar")
+$user[1] = "amellya";
+
+// mencetak isi array
+echo "<pre>";
+print_r($user);
+echo "</pre>";
+
+
+echo "<p><strong>By muhammad ilham</strong>";
+?>
