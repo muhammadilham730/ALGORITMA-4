355537bb13f043f258440b5394599d6c09d018e0
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 08:40:58 +0700

    41 assosiatif array

---
+++ 41 assosiatif array.php
@@ -0,0 +1,8 @@
+
+<?php
+$kendaraan = array('jenis' => 'Mobil', 'merk' => 'Toyota', 'tipe' => 'Vios');
+foreach ($kendaraan as $key => $val) {
+	echo ucfirst($key) . ': ' . $val . '<br/>';
+}
+
+echo "<p><strong>By muhammad ilham</strong>";
