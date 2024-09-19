2198b60393f42b085657710a5401a6ac76958e65
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 09:24:56 +0700

    59 array splice

---
+++ 59 array splice.php
@@ -0,0 +1,8 @@
+<?php
+echo "<pre>";
+$buah = ["apel", "jeruk", "pisang", "mangga"];
+array_splice($buah, 2, 1, "kiwi");
+print_r($buah);
+echo "<pre>";
+
+echo "<p><strong>By muhammad ilham</strong>";
