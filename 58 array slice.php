0cd9c48ed235c7e13766c867913b0b3f5ac09a49
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 09:23:21 +0700

    58 array slice.php

---
+++ 58 array slice.php
@@ -0,0 +1,8 @@
+<?php
+echo "<pre>";
+$buah = ["apel", "jeruk", "pisang", "mangga"];
+$subBuah = array_slice($buah, 1, 2);
+print_r($subBuah);
+echo "<pre>";
+
+echo "<p><strong>By nuhammad ilham</strong>";
