151319de97bdd15a95a2ef14fe243721de295559
muhammadilham730 <mi8190044@gmail.com>
2024-09-19 08:38:00 +0700

    40 penggunaan index pada array

---
+++ 40 penggunaan index pada array.php
@@ -0,0 +1,7 @@
+<?php
+$bulan = array ('Januari', 'Februari', 'Maret');
+foreach ($bulan as $index => $nama_bulan) {
+	echo ($index + 1) . '. ' . $nama_bulan . '<br/>';
+}
+ 
+echo "<p><strong>By muhammad ilham</strong>";
